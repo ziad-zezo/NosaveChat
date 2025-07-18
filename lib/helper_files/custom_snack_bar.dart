59f9bdb215fc304443bf05e20bb021b953b59cd7
ziad-zezo <20224264@fue.edu.eg>
2025-07-18 21:28:54 +0300

    feat: Add CustomSnackBar, AboutScreen, and ProGuard rules for ML Kit; remove AppEntryPoint

---
+++ lib/helper_files/custom_snack_bar.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class CustomSnackBar {
+  static void showErrorSnackBar(
+    BuildContext context, {
+    required String message,
+    IconData? icon,
+  }) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      snackBar(
+        context: context,
+        message: message,
+        color: Colors.red,
+        icon: icon??Icons.report_problem_outlined,
+      ),
+    );
+  }
+
+  static void showWarningSnackBar() {}
+  static void showInfoSnackBar(
+    BuildContext context, {
+    required String message,
+    IconData? icon,
+  }) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      snackBar(
+        context: context,
+        message: message,
+        color: Colors.grey,
+        icon: icon??Icons.info,
+      ),
+    );
+  }
+
+  static void showSuccessSnackBar(
+    BuildContext context, {
+    required String message,
+    IconData? icon,
+  }) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      snackBar(
+        context: context,
+        message: message,
+        color: Colors.green,
+        icon: icon??Icons.check_circle,
+      ),
+    );
+  }
+
+  static SnackBar snackBar({
+    required BuildContext context,
+    required String message,
+    required IconData icon,
+    required Color color,
+  }) {
+    return SnackBar(
+      duration: Duration(milliseconds: 2500),
+      elevation: 0,
+      dismissDirection: DismissDirection.horizontal,
+      behavior: SnackBarBehavior.floating,
+      margin: EdgeInsets.symmetric(horizontal: 12, vertical: 20),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+        side: BorderSide(color: color, width: 2),
+      ),
+      backgroundColor: Colors.black87,
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 20),
+      content: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Icon(icon, color: color, size: 26),
+          ),
+          Text(
+            message,
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 1.25,
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
